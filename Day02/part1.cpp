dc024754958ad1a7d32a1d8cb979444a7a6812d5
slaierno <simonelaierno@gmail.com>
2022-12-02 10:31:56 +0100

    Day02 solution

---
+++ Day02/part1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <range/v3/all.hpp>
+#include "utils.hpp"
+
+class RPS {
+    int m_rps;
+    constexpr static int ROCK = 0;
+    constexpr static int PAPER = 1;
+    constexpr static int SCISSOR = 2;
+public:
+    constexpr RPS(char c) : m_rps(c - (c < 'X' ? 'A' : 'X')) {}
+    constexpr auto operator<=>(const RPS& rhs) const {
+        if (m_rps == rhs.m_rps) return 0;
+        else if ((m_rps + 1) % 3 == rhs.m_rps) return -1;
+        else return 1;
+    }
+    constexpr auto score() const { return m_rps + 1; }
+};
+
+using Round = std::pair<RPS, RPS>;
+
+uint64_t get_round_score(const Round& round) {
+    return 3*((round.second <=> round.first) + 1) + round.second.score();
+}
+
+//13526
+int main(int argc, char *argv[])
+{
+    using namespace ranges;
+
+    //quick and dirty
+    assert(argc == 2);
+
+    auto score = accumulate( 
+        AoC::get_input(argv[1], "\n", [](auto v){
+            return get_round_score({v[0], v[2]});
+        }),
+        0ULL);
+
+    std::cout << score << std::endl;
+
+    return 0;
+}
