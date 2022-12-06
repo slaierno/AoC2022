bc171d5c235d54b496c9ce0b8d24ef1a1727dee6
slaierno <simonelaierno@gmail.com>
2022-12-06 15:08:39 +0100

    added optimized alternative solutions to Day06

--- Day06/part1_alt.cpp
+++ Day06/part1_alt.cpp
@@ -2,22 +2,33 @@
 #include <fstream>
 #include <iostream>
 #include <range/v3/all.hpp>
+#include <set>
 
 #include "utils.hpp"
 
-// 1300
-int main(int argc, char* argv[]) {
-    using namespace ranges;
+using namespace ranges;
 
+bool is_unique_c26(const ranges::input_range auto& window) {
+    std::array<char, 26> cs{};
+    return window.end() ==
+           find_if(window, [&cs](auto c) { return ++cs[c - 'a'] > 1; });
+}
+bool is_unique(const ranges::input_range auto& window) {
+    std::set<char> cs{};
+    return window.end() ==
+           find_if(window, [&cs](auto c) { return !cs.insert(c).second; });
+}
+
+// 3986
+int main(int argc, char* argv[]) {
     // quick and dirty
     assert(argc == 2);
 
     constexpr size_t marker_size = 4;
     const auto input = AoC::get_input(argv[1]);
-    for (auto [i, window] :
+    for (const auto& [i, window] :
          input | views::sliding(marker_size) | views::enumerate) {
-        if (size(window) == size(window | to<std::vector>() | actions::sort |
-                                 actions::unique)) {
+        if (is_unique_c26(window)) {
             std::cout << i + marker_size << std::endl;
             return 0;
         }
